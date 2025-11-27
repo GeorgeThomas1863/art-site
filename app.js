0ba2ee50a7a07b369ba053a4a58760758f62f968
George H. Thomas <georgebushdontcare@gmail.com>
2025-11-27 15:03:45 -0500

    start

---
+++ app.js
@@ -0,0 +1,33 @@
+import express from "express";
+import session from "express-session";
+import routes from "./routes/router.js";
+
+import CONFIG from "./config/config.js";
+
+//const { expressPicPath, expressVidPath, expressWatchPath, expressConfigPublicPath, picPath, vidPath, watchPath, scrapePort, displayPort } = CONFIG;
+const { displayPort } = CONFIG;
+
+const app = express();
+
+//claude solution to auth problem
+// app.set("trust proxy", 1);
+
+// app.use(session(CONFIG.buildSessionConfig()));
+
+//custom paths to expose to frontend
+// app.use(expressPicPath, express.static(picPath));
+// app.use(expressVidPath, express.static(vidPath));
+// app.use(expressWatchPath, express.static(watchPath));
+// app.use(expressConfigPublicPath, express.static("config/config-public.js"));
+
+//standard public path
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//routes
+app.use(routes);
+
+// app.listen(1801);
+app.listen(displayPort);
---
+++ routes/router.js
@@ -0,0 +1,47 @@
+import express from "express";
+
+import CONFIG from "../config/config.js";
+import { requireAuth, requireAdminAuth } from "./auth.js";
+import { authController, adminAuthController } from "../controllers/auth-controller.js";
+import { mainDisplay, adminDisplay, display404, display500, display401 } from "../controllers/display-controller.js";
+
+// const {
+//   updateDisplayDataRoute,
+//   adminAuthRoute,
+//   adminCommandRoute,
+//   adminDataRoute,
+//   adminPollingRoute,
+// } = CONFIG;
+
+const router = express.Router();
+
+// Login AUTH route
+// router.post("/site-auth-route", authController);
+// router.post(adminAuthRoute, adminAuthController);
+// router.get("/401", display401);
+
+//-----------------------------
+
+// router.post("/get-backend-value-route", requireAuth, getBackendValueController);
+
+// router.post(updateDisplayDataRoute, requireAuth, updateDisplayDataController);
+
+//-----------------------------
+
+// router.post(adminDataRoute, requireAdminAuth, adminDataController);
+
+// //send data to scraper
+// router.post(adminCommandRoute, requireAdminAuth, adminCommandController);
+
+// //poll backend
+// router.post(adminPollingRoute, requireAdminAuth, adminPollingController);
+
+// router.use("/admin", requireAdminAuth, adminDisplay);
+
+router.get("/", mainDisplay);
+
+router.use(display404);
+
+router.use(display500);
+
+export default router;
