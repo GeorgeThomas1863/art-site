5ad55486280fbc66dbda3ea1fa69921d9e6a9d6d
GeorgeThomas1863 <georgebushdontcare@gmail.com>
2025-12-19 11:48:53 -0500

    switching to name

--- public/js/util/params.js
+++ public/js/util/params.js
@@ -1,7 +1,7 @@
 export const getNewProductParams = async () => {
   const params = {
     productType: document.getElementById("product-type").value,
-    title: document.getElementById("name").value,
+    name: document.getElementById("name").value,
     price: document.getElementById("price").value,
     description: document.getElementById("description").value,
     picData: document.getElementById("upload-button").uploadData,
