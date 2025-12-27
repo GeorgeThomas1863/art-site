ea247ff84d67701b954995890456182a4005244a
GeorgeThomas1863 <georgebushdontcare@gmail.com>
2025-12-27 11:37:40 -0500

    refactor

--- app.js
+++ app.js
@@ -29,6 +29,7 @@
 import express from "express";
 import session from "express-session";
 import routes from "./routes/router.js";
+import adminRoutes from "./routes/admin-routes.js";
 import cartRoutes from "./routes/cart-routes.js";
 
 import CONFIG from "./config/config.js";
@@ -50,6 +51,7 @@ app.use(express.json());
 
 //routes
 app.use(routes);
+app.use(adminRoutes);
 app.use(cartRoutes);
 
 app.listen(port);
--- controllers/cart-controller.js
+++ controllers/cart-controller.js
@@ -91,6 +91,8 @@ export const updateCartItem = async (req, res) => {
 export const removeFromCart = async (req, res) => {
   await buildCart(req);
 
+  const { productId } = req.params;
+
   let newCart = [];
   for (let i = 0; i < req.session.cart.length; i++) {
     if (req.session.cart[i].productId !== productId) {
