7da1e44ed5d3267640059165310499c46e628366
GeorgeThomas1863 <georgebushdontcare@gmail.com>
2025-12-27 21:07:24 -0500

    dev

--- public/js/util/popup.js
+++ public/js/util/popup.js
@@ -4,8 +4,6 @@ const productsElement = document.getElementById("products-element");
 const cartElement = document.getElementById("cart-element");
 
 export const displayPopup = async (message, type = "success") => {
-  if (!adminElement) return null;
-
   console.log("DISPLAY POPUP");
   console.log(message);
   console.log(type);
