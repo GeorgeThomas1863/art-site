6807f1f0f87514fabd4a0fbcebf5777c6fee83af
GeorgeThomas1863 <georgebushdontcare@gmail.com>
2026-03-28 12:33:40 -0400

    local env mistake

--- app.js
+++ app.js
@@ -13,7 +13,7 @@
 import dotenv from "dotenv";
 
 dotenv.config({ path: ".env" });
-dotenv.config({ path: ".env.local" });
+dotenv.config({ path: ".env.local", override: true });
 
 import express from "express";
 import session from "express-session";
